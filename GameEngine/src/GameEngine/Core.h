7bfb3067e173d9c53603a2f204993efaf40bd0be
s21yusuforung <yusuforung@sabanciuniv.edu>
2021-12-01 03:10:26 +0300

    Basic Application Setup and Entry Point

---
+++ GameEngine/src/GameEngine/Core.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifdef GE_PLATFORM_WINDOWS
+	#ifdef GE_BUILD_DLL
+		#define GameEngine_API __declspec(dllexport)
+	#else
+		#define GameEngine_API __declspec(dllimport)
+	#endif // GE_BUILD_DLL
+#else
+	#error Game Engine only supports Windows
+#endif // GE_PLATFORM_WINDOWS
